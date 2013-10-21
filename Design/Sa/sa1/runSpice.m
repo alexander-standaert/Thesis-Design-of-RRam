b1453a9f5985379c0eccf21e482c8792570794f5
WouterDiels <sfdjkl@sdfj.clf>
2013-10-21 12:32:48 +0200

    Maple file, plots aangepast

--- Design/Sa/sa1/runSpice.m
+++ Design/Sa/sa1/runSpice.m
@@ -88,15 +88,15 @@ spicepath = strcat(strrep(currentpath,pwd,''),'/spice');
 steptime = 10^-12;
 stoptime = 10*10^-9;
 
-sel1_1 = wavegen([1e-9,4.5e-9;1,0],0.1e-9,0.05e-9,0,1,6e-9); %pmos gate
+sel1_1 = wavegen([0,4e-9;1,0],0.1e-9,0.05e-9,0,1,6e-9); %pmos gate
 sel1_2 = sel1_1;
 sel1_3 = sel1_1;
 
-sel2_1 = wavegen([0,2e-9,4e-9;0,1,0],0.1e-9,0.05e-9,0,1,6e-9); %nmos gate
+sel2_1 = wavegen([0,0.5e-9,2e-9;0,1,0],0.1e-9,0.05e-9,0,1,6e-9); %nmos gate
 sel2_2 = sel2_1;
 sel2_3 = sel2_1;
 
-wl_1 = wavegen([0,5e-9;0,1],0.1e-9,0.05e-9,0,1,5.5e-9); %nmos gate
+wl_1 = wavegen([0,3e-9;0,1],0.1e-9,0.05e-9,0,1,5.5e-9); %nmos gate
 wl_2 = wl_1;
 wl_3 = wl_1;
 
@@ -111,7 +111,7 @@ vload_3 = 1;
 LE1 = wavegen([0,7.2e-9;1,0],0.1e-9,0.05e-9,0,1,5e-9); %pmos gate
 LE2 = wavegen([0,7.2e-9;0,1],0.1e-9,0.05e-9,0,1,5e-9); %nmos gate
 
-selL1 = wavegen([0,6.5e-9,6.7e-9;0,1,0],0.1e-9,0.05e-9,0,1,5e-9);
+selL1 = wavegen([0,6.5e-9,6.7e-9;0,0,0],0.1e-9,0.05e-9,0,1,5e-9);
 selL2 = selL1;					
 
 if mismatch_on == 1
@@ -136,23 +136,23 @@ system('spectre -format psfascii ./Sa/sa1/spice/sa1_testbench.sp')
 
 %plot control signals
 sel1_1 = sim.getSignal('sel1_1');
-sel1_1x = sel1_1.getXValues;
+sel1_1x = sel1_1.getXValues*10^9;
 sel1_1y = sel1_1.getYValues;
 
 sel2_1 = sim.getSignal('sel2_1');
-sel2_1x = sel2_1.getXValues;
+sel2_1x = sel2_1.getXValues*10^9;
 sel2_1y = sel2_1.getYValues;
 
 wl_1 = sim.getSignal('wl_1');
-wl_1x = wl_1.getXValues;
+wl_1x = wl_1.getXValues*10^9;
 wl_1y = wl_1.getYValues;
 
 LE1 = sim.getSignal('LE1');
-LE1x = LE1.getXValues;
+LE1x = LE1.getXValues*10^9;
 LE1y = LE1.getYValues;
 
 selL1 = sim.getSignal('selL1');
-selL1x = selL1.getXValues;
+selL1x = selL1.getXValues*10^9;
 selL1y = selL1.getYValues;
 
 
@@ -161,38 +161,44 @@ figure
 hold on
 plot(sel1_1x,(sel1_1y)*-1+2.1,sel2_1x,sel2_1y+2.2,wl_1x,wl_1y+3.3,LE1x,LE1y+4.4,selL1x,selL1y+5.5)
 hleg1 = legend('selectline1*-1','selectline2','wordline','latch enable','select latch','Location','SouthEastOutside');
+xlabel('time [ps]')
+ylabel('voltage [V]')
 hold off
 
 %plot bitline
 
 bl_1 = sim.getSignal('bl_1');
-bl_1x = bl_1.getXValues;
+bl_1x = bl_1.getXValues*10^9;
 bl_1y = bl_1.getYValues;
 
 bl_2 = sim.getSignal('bl_2');
-bl_2x = bl_2.getXValues;
+bl_2x = bl_2.getXValues*10^9;
 bl_2y = bl_2.getYValues;
 
 figure
 hold on
 plot(bl_1x,bl_1y,bl_2x,bl_2y)
 hleg1 = legend('bitline1','bitline2','Location','SouthEastOutside');
+xlabel('time [ps]')
+ylabel('voltage [V]')
 hold off
 
 %plot output
 
 out = sim.getSignal('out');
-outx = out.getXValues;
+outx = out.getXValues*10^9;
 outy = out.getYValues;
  
 outb = sim.getSignal('outbar');
-outbx = outb.getXValues;
+outbx = outb.getXValues*10^9;
 outby = outb.getYValues;
  
 figure
 hold on
 plot(outx,outy,outbx,outby)
 hleg1 = legend('out','outbar','Location','SouthEastOutside');
+xlabel('time [ps]')
+ylabel('voltage [V]')
 hold off
 
  
