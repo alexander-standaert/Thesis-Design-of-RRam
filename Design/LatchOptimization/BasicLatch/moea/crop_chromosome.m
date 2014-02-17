26255cdd4299c23b587fea9bd3fd43e6a874c8f8
alexander-standaert <alexander.standaert@student.kuleuven.be>
2014-02-17 09:35:49 +0100

    code cleanup

--- Design/LatchOptimization/BasicLatch/moea/crop_chromosome.m
+++
@@ -1,8 +0,0 @@
-function chromosome = crop_chromosome(intermediate_chromosome,pop)
-
-[~,s_end] = size(intermediate_chromosome);
-intermediate_chromosome = sortrows(intermediate_chromosome,[s_end-1 -s_end]);
-
-chromosome = intermediate_chromosome(1:pop,:);
-
-end
--- Design/memristor_model/netlists/hourglass_examples/setreset_sweep/cleanIt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf generated
-rm -f  m2s_run.m
-rm -f  *m~ 
