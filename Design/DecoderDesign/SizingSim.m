80956149ee48751cd1ebb1beba14d81ba0579185
WouterDiels <sfdjkl@sdfj.clf>
2014-03-13 10:49:34 +0100

    Veranderde OptimizeDecoder

--- Design/DecoderDesign/SizingSim.m
+++ Design/DecoderDesign/SizingSim.m
@@ -7,8 +7,9 @@ function [] = SizingSim(process_id)
     data = load(strjoin({'./DecoderDesign/OptimizeDecoder/inputs/inputs_',num2str(process_id),'.mat'},''));
     mega = data.data;
     
-    for i = 1:length(mega)
-        [~,t,e]=simdata(mega(i,1),mega(i,3),mega(i,4),mega(i,2));
+    for z = 1:length(mega)
+        z
+        [~,t,e]=simdata(mega(z,2),mega(z,3),mega(z,1));
         if isempty(t)
             blacklist = 1;
             energy=0;
@@ -18,74 +19,31 @@ function [] = SizingSim(process_id)
             energy=e;
             delay=t;
         end
-        datamatrix = [mega(i,:),delay,energy,blacklist];
-        save(strjoin({'./DecoderDesign/OptimizeDecoder/outputs/outputs_',num2str(mega(i,1)),'_',num2str(mega(i,2)),'_',num2str(mega(i,3)),'_',num2str(mega(i,4)),'.mat'},''),'datamatrix')
+        datamatrix = [mega(z,:),delay,energy,blacklist];
+        save(strjoin({'./DecoderDesign/OptimizeDecoder/outputs/outputs_',num2str(z),'.mat'},''),'datamatrix')%save(strjoin({'./DecoderDesign/OptimizeDecoder/outputs/outputs_',num2str(mega(i,1)),'_',num2str(mega(i,2)),'_',num2str(mega(i,3)),'.mat'},''),'datamatrix')
     end
     
-    function [sim,delay,energy] = simdata(NoI,NoIS,multfac,NoBLpLB)
+    function [sim,delay,energy] = simdata(NoIS,multfac,NoBLpLB)
         %%
-        % NoI=10;
-        sp.NoI=NoI;
-        sp.NoISTRING = int2spelledstring(NoI);
+
         sp.CL = 0.18e-15*NoBLpLB;
         
         sp.MismatchOn=0;
         sp.numruns=1;
-        sp.simlength=3e-9;   %2^sp.NoI*3e-9;
+        sp.simlength=3e-9;
         
         sp.NoIS = NoIS;
-        
-        testvectorin = zeros(2^sp.NoI,sp.NoI);
-        
-        sp.sizingcell = cell(decoderparameters(NoI),1);
-        for i=1:decoderparameters(NoI)
-            sp.sizingcell{i} = 1^((decoderparameters(NoI)-i)/decoderparameters(NoI));
-        end
-        
-        sp.sizingcell{1}=sp.sizingcell{1}/5*3;
-        % sp.sizingcell{2}=80;
-        % sp.sizingcell{3}=1;
-        % sp.sizingcell{4}=1;
-        % sp.sizingcell{5}=1;
-        % sp.sizingcell{6}=1;
         
         sp.invsize = cell(sp.NoIS,1);
         for i=1:sp.NoIS
             sp.invsize{i}=multfac^((i-1)/sp.NoIS);
         end
         
-        
-        j=1;
-        for i=2:2^sp.NoI
-            jstr=dec2bin(j,sp.NoI);
-            for k=1:sp.NoI
-                testvectorin(i,k)=str2num(jstr(k));
-            end
-            j=j+1;
-        end
-        
-        wavein = cell(sp.NoI,1);
-        for i=1:sp.NoI
-            wavetempgroup=[];
-            for k=3             %1:2^sp.NoI
-                wavetemp = makewave(strcat('wave',num2str(i)),[1,1,1]*1e-9,[0,testvectorin(k,sp.NoI+1-i),0]);
-                wavetempgroup = makewavegroup('tempgroup',[wavetemp]);
-                wavetempgroups(1) = wavetempgroup;
-            end
-            wave = calcwaves(wavetempgroups);
-            wavein{i}=getfield(wave,strcat('wave',num2str(i)));
-        end
-        clear wave
-        wavetempgroup=[];
-        for k=1         %:2^sp.NoI
-            wavetemp = makewave('enable',[1,1,1]*1e-9,[0,1,0]);
-            wavetempgroup = makewavegroup('tempgroup',[wavetemp]);
-            wavetempgroups(k) = wavetempgroup;
-        end
-        wave = calcwaves(wavetempgroups);
-        
-        sp.enablewave = wave.enable;
-        sp.wavesin = wavein;
+        wavetemp = makewave('in',[1,1,1]*1e-9,[1,0,1]);
+        wavetempgroup = makewavegroup('tempgroup',[wavetemp]);
+        wave = calcwaves(wavetempgroup);
+
+        sp.wavein = wave.in;
         
         clear wave
         
@@ -123,7 +81,7 @@ function [] = SizingSim(process_id)
         
         [sim] = readPsfAscii(strjoin({'/tmp/',param.rnddirname,'/spice/sizingsim.raw/mymc-001_mytran.tran'},''), '.*');
         
-        a=sim.getSignal('OUT_2');
+        a=sim.getSignal('OUT');
         v=a.getYValues;
         t=a.getXValues;
         [~, tstart] = min(abs(t - 1e-9));
